7a105ffff7b183c3e84f34a5728870787f5d6c86
govsandhu <gc.sandhu@gmail.com>
2019-07-02 17:58:47 +0000

    Set intial state within App. Created function to verify if currentUser is not null, and render current user in ChatBar

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,26 @@ import ChatBar from './ChatBar.jsx';
 import MessageList from './MessageList.jsx';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+       // optional. if currentUser is not defined, it means the user is Anonymous
+      currentUser: {name: 'Bob'},
+      messages: [
+        {
+          username: 'Bob',
+          content: 'Has anyone seen my marbles?',
+        },
+        {
+          username: 'Anonymous',
+          content: 'No, I think you lost them. You lost your marbles Bob. You lost them for good.'
+        }
+      ]
+    }
+  }
+
   render() {
     return (
       <div>
@@ -12,7 +32,7 @@ class App extends Component {
           </a>
         </nav>
         <MessageList />
-        <ChatBar />
+        <ChatBar currentUser={this.state.currentUser}/>
       </div>
     );
   }
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
 
 class ChatBar extends Component {
-  // constructor(props) {
-  //   super(props)
-  // }
-  
+  constructor(props) {
+    super(props)
+  }
+
+checkCurrentUser () {
+  if(this.props.currentUser) {
+    return (
+      <input className="chatbar-username" defaultValue={this.props.currentUser.name} />
+    )
+    // this.props.currentUser.name
+  } else {
+    return (
+      <input className="chatbar-username" placeholder="Annoymous" />
+    )
+    // 'Annoymous'
+  }
+}
+  // return the tag you want
   render() {
     return (
         <footer className="chatbar">
-        <input className="chatbar-username" placeholder="Your Name (Optional)" />
+        {this.checkCurrentUser()}
         <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
         </footer>
-    )}
+    )
+  }
 }
 
 export default ChatBar;
