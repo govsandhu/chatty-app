86806cab8c09a014bbea7e5cef8fbe38280ab985
govsandhu <gc.sandhu@gmail.com>
2019-07-03 18:48:17 +0000

    Console log message on successful connection to socket

--- src/App.jsx
+++ src/App.jsx
@@ -27,16 +27,23 @@ class App extends Component {
   }
 
   componentDidMount() {
-    console.log('componentDidMount <App />');
-    setTimeout(() => {
-      console.log('Simulating incoming message');
-      // Add a new message to the list of messages in the data store
-      const newMessage = {id: 3, username: 'Michelle', content: 'Hello there!'};
-      const messages = this.state.messages.concat(newMessage)
-      // Update the state of the app component.
-      // Calling setState will trigger a call to render() in App and all child components.
-      this.setState({messages: messages})
-    }, 3000);
+    this.webSocket = new WebSocket('ws://localhost:3001')
+
+    this.webSocket.onopen = function (event) {
+      console.log('Client connected')
+    }
+    
+    console.log('Connected to server')
+    // console.log('componentDidMount <App />');
+    // setTimeout(() => {
+    //   console.log('Simulating incoming message');
+    //   // Add a new message to the list of messages in the data store
+    //   const newMessage = {id: 3, username: 'Michelle', content: 'Hello there!'};
+    //   const messages = this.state.messages.concat(newMessage)
+    //   // Update the state of the app component.
+    //   // Calling setState will trigger a call to render() in App and all child components.
+    //   this.setState({messages: messages})
+    // }, 3000);
   }
 
   addNewMessage (message) {
